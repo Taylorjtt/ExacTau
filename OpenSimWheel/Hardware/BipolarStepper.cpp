93ea33428ccfa9a7869476bec05d5df9066ad279
taylorjtt <taylorjtt@gmail.com>
2018-09-04 15:33:15 -0600

    Added a Bipolar Steper Class -implements microstepping -adds functionality to home and zero a motor fitted with an encoder

---
+++ OpenSimWheel/Hardware/BipolarStepper.cpp
@@ -0,0 +1,117 @@
+/*
+ * BipolarStepper.cpp
+ *
+ *  Created on: Aug 30, 2018
+ *      Author: John
+ */
+
+#include "BipolarStepper.h"
+
+
+
+BipolarStepper::BipolarStepper()
+{
+	// TODO Auto-generated constructor stub
+}
+BipolarStepper::BipolarStepper(OSWInverter inverter)
+{
+	this->inv = inverter;
+	thetaE = 0.0;
+	thetaEIncrement = 0.00314;
+	speed = 5;
+	multiplier = 0.5;
+	vAlpha = 0.0;
+	vBeta = 0.0;
+	max = 0.0;
+	min = 0.0;
+	c = 0.0;
+	a = 0.0;
+	b = 0.0;
+
+}
+void BipolarStepper::zero(QuadratureEncoder &encoder)
+{
+
+	microstepCW(100000);
+	int32_t distance = (int32_t)encoder.getRawTicks() - 8545;
+	if(distance < 0)
+	{
+		while(distance < 0)
+		{
+			microstepCCW();
+			DELAY_US(speed);
+			distance = (int32_t)encoder.getRawTicks() - 8545;
+		}
+	}
+	else
+	{
+		while(distance > 0)
+		{
+			microstepCW();
+			DELAY_US(speed);
+			distance = (int32_t)encoder.getRawTicks() - 8545;
+		}
+	}
+	DELAY_US(1e6);
+	inv.modulate(34.3,63.5417,34.3);
+	DELAY_US(1e6);
+	encoder.zero();
+	DELAY_US(1e6);
+	inv.modulate(0.0,0.0,0.0);
+}
+void BipolarStepper::microstepCW()
+{
+	thetaE = fmod(thetaE + thetaEIncrement,2*MATH_PI);
+	vAlpha = 7.5*sin(thetaE);
+	vBeta = 7.5*cos(thetaE);
+	max = MAX(vAlpha, vBeta);
+	min = MIN(vAlpha, vBeta);
+	c =  0.5 * (24.0 - (max-min));
+	a = c + vAlpha;
+	b = c + vBeta;
+	c = 4.1666*c;
+	a = 4.1666*a;
+	b = 4.1666*b;
+	inv.modulate(multiplier*a,multiplier*b,multiplier*c);
+}
+void BipolarStepper::microstepCCW()
+{
+	thetaE = fmod(thetaE - thetaEIncrement,2*MATH_PI);
+	vAlpha = 7.5*sin(thetaE);
+	vBeta = 7.5*cos(thetaE);
+	max = MAX(vAlpha, vBeta);
+	min = MIN(vAlpha, vBeta);
+	c =  0.5 * (24.0 - (max-min));
+	a = c + vAlpha;
+	b = c + vBeta;
+	c = 4.1666*c;
+	a = 4.1666*a;
+	b = 4.1666*b;
+	inv.modulate(multiplier*a,multiplier*b,multiplier*c);
+
+}
+
+void BipolarStepper::microstepCW(uint32_t numberOfSteps)
+{
+	for(uint32_t i = 0; i < numberOfSteps; i ++)
+	{
+		microstepCW();
+		DELAY_US(speed);
+	}
+
+}
+void BipolarStepper::microstepCCW(uint32_t numberOfSteps)
+{
+	for(uint32_t i = 0; i < numberOfSteps; i ++)
+	{
+		microstepCCW();
+		DELAY_US(speed);
+	}
+
+}
+
+BipolarStepper::~BipolarStepper() {
+	// TODO Auto-generated destructor stub
+}
+
+
---
+++ OpenSimWheel/Hardware/BipolarStepper.h
@@ -0,0 +1,46 @@
+/*
+ * BipolarStepper.h
+ *
+ *  Created on: Aug 30, 2018
+ *      Author: John
+ */
+
+#ifndef HARDWARE_BIPOLARSTEPPER_H_
+#define HARDWARE_BIPOLARSTEPPER_H_
+#include "Hardware.hh"
+#include "../math.h"
+#include "OSWHardware.hh"
+
+
+
+
+class BipolarStepper {
+public:
+	BipolarStepper();
+	BipolarStepper(OSWInverter inverter);
+	void microstepCW();
+	void microstepCCW();
+	void microstepCW(uint32_t numberOfSteps);
+	void microstepCCW(uint32_t numberOfSteps);
+	void initializeEncoder();
+	void zero(QuadratureEncoder &encoder);
+	virtual ~BipolarStepper();
+private:
+	OSWInverter inv;
+	float thetaE;
+	float thetaEIncrement;
+	float vAlpha;
+	float vBeta;
+	float max;
+	float min;
+	float c;
+	float a;
+	float b;
+	float stepsPerSecond;
+	int speed;
+	float multiplier;
+};
+
+
+
+#endif /* HARDWARE_BIPOLARSTEPPER_H_ */
